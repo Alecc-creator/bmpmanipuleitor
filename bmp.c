3dcca66fb02957f6e68c608cfd1ae19cb0892dc0
Alejandro <acuevascardozo@alumno.unlam.edu.ar>
2026-09-19 15:17:59 -0300

    Agrega validaciones iniciales de archivos BMP

--- bmp.c
+++ bmp.c
@@ -1,1 +1,122 @@
 #include "bmp.h"
+
+int validarArchivoLectura(const char* nomArch)
+{
+    FILE* arch;
+
+    arch = fopen(nomArch, "rb");
+    if(!arch)
+    {
+        printf("ERROR: No se pudo abrir el archivo %s.\n", nomArch);
+        return ERROR_ARCHIVO;
+    }
+
+    fclose(arch);
+
+    return TODO_OK;
+}
+
+int validarBMP(const char* nomArch)
+{
+    FILE* arch;
+    CabeceraBMP cabecera;
+    int ret;
+
+    arch = fopen(nomArch, "rb");
+    if(!arch)
+    {
+        printf("ERROR: No se pudo abrir el archivo %s.\n", nomArch);
+        return ERROR_ARCHIVO;
+    }
+
+    ret = leerCabeceraBMP(arch, &cabecera);
+    if(ret != TODO_OK)
+    {
+        fclose(arch);
+        return ret;
+    }
+
+    if(cabecera.ancho < 1 || cabecera.alto == 0)
+    {
+        printf("ERROR: Dimensiones invalidas.\n");
+        fclose(arch);
+        return ERROR_BMP_INVALIDO;
+    }
+
+    if(cabecera.planos != 1)
+    {
+        printf("ERROR: Plano invalido.\n");
+        fclose(arch);
+        return ERROR_BMP_INVALIDO;
+    }
+
+    if(cabecera.bitsPorPixel != PROFUNDIDAD)
+    {
+        printf("ERROR: La profundidad debe ser de %d bits.\n", PROFUNDIDAD);
+        fclose(arch);
+        return ERROR_BMP_INVALIDO;
+    }
+
+    if(cabecera.compresion != 0)
+    {
+        printf("ERROR: El archivo NO debe ser comprimido.\n");
+        fclose(arch);
+        return ERROR_BMP_INVALIDO;
+    }
+
+    fclose(arch);
+
+    return TODO_OK;
+}
+
+int leerCabeceraBMP(FILE* arch, CabeceraBMP* cabecera)
+{
+    unsigned char firma[TAM_FIRMA_BMP];
+
+    if(fseek(arch, 0, SEEK_SET) != 0)
+        return ERROR_ARCHIVO;
+
+    if(fread(firma, sizeof(unsigned char), TAM_FIRMA_BMP, arch) != 2)
+        return ERROR_BMP_INVALIDO;
+
+    if(firma[0] != 'B' || firma[1] != 'M')
+    {
+        printf("ERROR: el archivo no es BMP.\n");
+        return ERROR_BMP_INVALIDO;
+    }
+
+    if(fseek(arch, OFFSET_TAMANIO_ARCHIVO, SEEK_SET) != 0)
+        return ERROR_ARCHIVO;
+
+    if(fread(&cabecera->tamArchivo, sizeof(uint32_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fseek(arch, OFFSET_INICIO_PIXELES, SEEK_SET) != 0)
+        return ERROR_ARCHIVO;
+
+    if(fread(&cabecera->offsetPixeles, sizeof(uint32_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fseek(arch, OFFSET_ANCHO, SEEK_SET) != 0)
+        return ERROR_ARCHIVO;
+
+    if(fread(&cabecera->ancho, sizeof(int32_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fseek(arch, OFFSET_PLANOS, SEEK_SET) != 0)
+        return ERROR_ARCHIVO;
+
+    if(fread(&cabecera->planos, sizeof(uint16_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fread(&cabecera->bitsPorPixel, sizeof(uint16_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fread(&cabecera->compresion, sizeof(uint32_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    if(fread(&cabecera->tamImagen, sizeof(uint32_t), 1, arch) != 1)
+        return ERROR_BMP_INVALIDO;
+
+    return TODO_OK;
+}
--- bmp.h
+++ bmp.h
@@ -2,6 +2,16 @@
 #define BMP_H_INCLUDED
 
 #include <stdint.h>
+#include <stdio.h>
+#include "codigos.h"
+
+#define PROFUNDIDAD             24
+#define TAM_FIRMA_BMP           2
+#define OFFSET_TAMANIO_ARCHIVO  2
+#define OFFSET_INICIO_PIXELES   10
+#define OFFSET_ANCHO            18
+#define OFFSET_PLANOS           26
+
 
 typedef struct
 {
@@ -10,5 +20,24 @@ typedef struct
     uint8_t azul;
 }Pixel;
 
+typedef struct
+{
+    uint32_t tamArchivo;
+    uint32_t offsetPixeles;
+
+    uint32_t ancho;
+    uint32_t alto;
+
+    uint16_t planos;
+    uint16_t bitsPorPixel;
+
+    uint32_t compresion;
+    uint32_t tamImagen;
+}CabeceraBMP;
+
+int validarArchivoLectura(const char* nomArch);
+int validarBMP(const char* nomArch);
+
+int leerCabeceraBMP(FILE* arch, CabeceraBMP* cabecera);
 
 #endif // BMP_H_INCLUDED
